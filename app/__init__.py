7bf098d23ccfce4efb83f349c2e1ef72bfb42c4b
maksonviini <maksonvinicio7@gmail.com>
2021-12-17 14:07:45 -0300

    Adicionando sqlite

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nvmeevmvuszlwq:9bc4228671430435b1a64d5003a4f59661c05f018ba22bd1e20c6cc82f11b050@ec2-3-226-165-146.compute-1.amazonaws.com:5432/df536mu06jkfig'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
 app.config['SECRET_KEY'] = 'secret'
 
 login_manager = LoginManager(app)
